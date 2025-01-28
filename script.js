91a488c23d2a140eaa1a3b15ad4b7640fb166ecc
Elena <157579304+Ellen010@users.noreply.github.com>
2025-01-28 14:06:51 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Function A
 function getAverage(scores) {
  let sum=0;
 for (let i=0; i<scores.length; i++) {
@@ -9,3 +10,29 @@ return average;
 
 console.log(getAverage([92, 88, 12, 77, 57, 100, 67, 38, 97, 89]));
 console.log(getAverage([45, 87, 98, 100, 86, 94, 67, 88, 94, 95]));
+
+// Function B
+
+function getGrade(score) {
+  let grade;
+  if (score === 100) {
+    grade = "A++";
+  } else if (score >= 90) {
+    grade = "A";
+  } else if (score >= 80 && score <= 89) {
+    grade = "B";
+  } else if (score >= 70 && score <= 79) { 
+    grade = "C";
+  } else if (score >= 60 && score <= 69) { 
+    grade = "D";
+  } else if (score >= 0 && score <= 59) {
+    grade = "F";
+  } else {
+    grade = "Invalid score";
+  }
+  return grade; 
+}
+
+console.log(getGrade(96)); 
+console.log(getGrade(82));
+console.log(getGrade(56)); 
